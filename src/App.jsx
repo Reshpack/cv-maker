d9b4da7f3d78f22b0e482312ecc9cc772fc75387
Suresh Packiry <sureshpackiry73@gmail.com>
2024-06-10 02:18:04 +1000

    formatting almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import { useEffect, useState } from 'react'
+import './styles/main.css'
 
-export default App() {
+
+export default function App() {
   return (
-    <h1 className="header">SPCV Maker</h1>
+    <>
+    <header className='header'>
+    <h1 className="header">SP CV Maker</h1>
+    </header>
 
-  );
-};
+    <div className='content'>
+    <div className='forms'>
+      <form>
+        <h2>Personal Details</h2>
+        <div>
+          <div>
+            <label for="full-name">
+              <span className='label-text'>Full Name</span>
+            </label>
+          </div>
+        </div>
+      </form>
+      <form>
+        <h2>Education</h2>
+        </form>
+      <form>
+        <h2>Work Experience</h2>
+        </form>
+    </div>
+    <div className='page'></div>
+    </div>
+    <footer></footer>
+    </>
+  )
+}
 
