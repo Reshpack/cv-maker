82b2163d86561c8e7b9357f1889124ad2e26e91a
Suresh Packiry <sureshpackiry73@gmail.com>
2024-06-18 22:17:00 +1000

    figuring out deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  "homepage": "https://github.com/Reshpack/cv-maker.git"
 })
